5e1b3f17a37f97eb3188dc9d0e1b710e1cb4e105
Ewen Piepers <ewen.piepers@ericsson.com>
2021-09-03 09:17:11 +0200

    initial commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,35 @@
+package com.mycompany.app;
+
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main(String[] args) throws IOException {
+        Terminal terminal = new DefaultTerminalFactory().createTerminal();
+        Screen screen = new TerminalScreen(terminal);
+
+        String s = "Hello World!";
+        TextGraphics tGraphics = screen.newTextGraphics();
+
+        screen.startScreen();
+        screen.clear();
+
+        tGraphics.putString(10, 10, s);
+        screen.refresh();
+
+        screen.readInput();
+        screen.stopScreen();
+    }
+
+
+}
